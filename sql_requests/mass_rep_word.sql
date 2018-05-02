a986e6a0bce9759eae349066d6bec1b259d663a0
Trish <38753765+Trish-Mundus@users.noreply.github.com>
2018-05-02 22:16:02 +0500

    Update mass_rep_word.sql

--- sql_requests/mass_rep_word.sql
+++ sql_requests/mass_rep_word.sql
@@ -1,2 +1,4 @@
 UPDATE `table` SET column_name = REPLACE(column_name, 'old word', 'new word') WHERE column_name LIKE "%another word%";
+/* EXAMPLE */
+UPDATE `oc_product_tab_content` a INNER JOIN `oc_product` b ON (a.product_id = b.product_id) SET a.content = 'text' WHERE a.product_id=b.product_id AND b.manufacturer_id = '54' AND a.tab_id='5';
 /* Mass replacement of the word in row*/
