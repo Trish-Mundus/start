ef2f0ccd0890dd6067d7f4825a2a04002ff5b5bb
Trish <38753765+Trish-Mundus@users.noreply.github.com>
2018-05-02 20:22:23 +0500

    Update add_column.sql

--- sql_requests/add_column.sql
+++ sql_requests/add_column.sql
@@ -1,1 +1,2 @@
-ALTER TABLE `realty_new` ADD COLUMN `type2` VARCHAR(50) NOT NULL
+ALTER TABLE `table_name` ADD COLUMN `column_name` VARCHAR(50) NOT NULL;
+ALTER TABLE `table_name` ADD COLUMN `column_name2` int(11) NOT NULL;
